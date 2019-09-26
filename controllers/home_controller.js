6da2ab7abeb516753338953a5081be2eca58312e
Nisha Maurya <nisha.maurya.2@gmail.com>
2019-09-27 04:47:33 +0530

    install ejs and add response pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    res.end('<h1>Express is up</h1>');
+    // res.end('<h1>Express is up</h1>');
+    return res.render('home',{
+        title: "home"
+    });
 }
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 
 app.use('/', require('./route'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 
 app.listen(port, function(err){
